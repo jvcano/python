0c40aba90d07eb1d6c3829ea91633c458d4bc15b
mimir <iorijvc@gmail.com>
2021-08-22 15:53:49 -0400

    ..

--- tip-calculator-start/main.py
+++ tip-calculator-start/main.py
@@ -1,6 +1,5 @@
-import colorama;
 from termcolor import colored;
-from colorama import Fore, Style;
+from colorama import Fore;
 
 #If the bill was $150.00, split between 5 people, with 12% tip. 
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
