18c8ea15739041d6a6813824b6200e27251a9ab8
mimir <iorijvc@gmail.com>
2021-08-26 22:28:03 -0400

    wut

--- day-10/day-10-start/main.py
+++ day-10/day-10-start/main.py
@@ -1,6 +1,8 @@
+from termcolor import colored;
+
 #function with outputs  
 def format_name(first_name,last_name):
- formated_first_name=first_name.upper()
+ formated_first_name=first_name.title()
  formated_last_name = last_name.title()
  return f"{formated_first_name} {formated_last_name}"
 def letter_number(name, last_name):
@@ -8,7 +10,10 @@ def letter_number(name, last_name):
   count_last_name = len(last_name)
   total = count_first_name + count_last_name
   return total
-formated_string = format_name("jose", "Cano")
-print(formated_string)
-total = letter_number("jose", "Cano")
-print(total)
+
+name = input("your first name ")
+last_name = input('your last name ')
+formated_string = format_name(name,last_name)
+print(formated_string,'green','on_red')
+total = letter_number(name,last_name)
+print(total,'green','on_red')
