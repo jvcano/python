d195632b542b66457d6c02514a882a5b76e3cecc
mimir <iorijvc@gmail.com>
2021-09-05 17:08:52 -0400

    loki new class

---
+++ day-10/calculador/calulator-1.py
@@ -0,0 +1,92 @@
+print(''' _____________________
+|  _________________  |
+| |              /  | |
+| |       /\    /   | |
+| |  /\  /  \  /    | |
+| | /  \/    \/     | |
+| |/             JO | |
+| |_________________| |
+|  __ __ __ __ __ __  |
+| |__|__|__|__|__|__| |
+| |__|__|__|__|__|__| |
+| |__|__|__|__|__|__| |
+| |__|__|__|__|__|__| |
+| |__|__|__|__|__|__| |
+| |__|__|__|__|__|__| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________| ''');
+
+def add(n1 ,n2):
+    """[this is the sum of 2 numbers]
+
+    Args:
+        n1 ([int]): [first number]
+        n2 ([int]): [second number]
+
+    Returns:
+        [int]: [result of the sum]
+    """
+    return n1+n2
+
+def substraction(n1,n2):
+    """[this is the subtraccion of two numbers only positive result ]
+
+    Args:
+        n1 ([int]): [first number]
+        n2 ([int]): [second number]
+
+    Returns:
+        [int]: [return of the substracion]
+    """
+    if n2>n1:
+        return n2-n1
+    else:
+        return n1-n2
+
+def multiplication(n1,n2):
+    """"multiplication of 2 numbers"""
+    return n1*n2
+
+def division (n1,n2):
+    """ Division of 2 numbers the biggers always gets diveded"""
+    if n2>n1:
+        return n2/n1
+    else:
+        return n1/n2  
+
+
+operations= {
+    "+":add,
+    "-":substraction,
+    "*":multiplication,
+    "/":division 
+}
+
+num1 = int(input("whats the first number: "))
+
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick an operation from the lines above: ")
+
+num2 = int(input("whats the second number: "))
+
+calculation_function = operations[operation_symbol]
+first_answer = calculation_function(num1,num2)
+
+print(f'{num1} {operation_symbol} {num2}= {first_answer}')
+
+#//!  This part is for a secondary operation and use of returns 
+operation_symbol = input("pick another operation: ")
+num3 = int(input("whats the next number: "))
+calculation_function = operations[operation_symbol]
+second_answer = calculation_function(first_answer,num3)
+
+print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
---
+++ day-10/day-10-1-2/functuins with outputs.py
@@ -0,0 +1,30 @@
+from termcolor import colored;
+
+#function with outputs  
+def format_name(first_name,last_name):
+    """[summary]
+
+    Args:
+        first_name ([type]): [description]
+        last_name ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+
+    formated_first_name=first_name.title()
+    formated_last_name = last_name.title()
+    return f"{formated_first_name} {formated_last_name}"
+
+def letter_number(name, last_name):
+  count_first_name = len(name)
+  count_last_name = len(last_name)
+  total = count_first_name + count_last_name
+  return total
+
+name = input("your first name ")
+last_name = input('your last name ')
+formated_string = format_name(name,last_name)
+print(formated_string,'green','on_red')
+total = letter_number(name,last_name)
+print(total,'green','on_red')
